049529a2bb1bf4aa2f4a8a74bf44a130cdc2870a
Jean-Claude Tarby <jean-claude.tarby@univ-lille.fr>
2022-03-08 17:58:13 +0100

    ajout de Peugeot

---
+++ src/unpackage/Peugeot.java
@@ -0,0 +1,13 @@
+package unpackage;
+
+public class Peugeot {
+    /**
+     * Peugeot est un constructeur automobile français filiale du groupe Stellantis depuis 2021. L'entreprise familiale qui précède l'actuelle entreprise Peugeot est fondée en 18102, lors de la transformation du moulin familial sis à Hérimoncourt en aciérie. Différentes branches de la famille vont se lancer dans des productions très diversifiées, mais toutes basées sur l'acier (outils, ressorts, baleines de parapluies, armatures de corsets, moulins à café, pièces d'horlogerie, bicyclettes, etc.) À partir de 1886, Armand Peugeot commence à s'intéresser à l'automobile. En 18962, il crée la « Société anonyme des automobiles Peugeot »3. Parallèlement, ses neveux Pierre, Robert et Jules se lancent en 1897 dans la fabrication de cycles sous le nom « Les fils de Peugeot frères » et produisent en 1906 leur première automobile. Afin d'éviter une concurrence interne nuisible à la production, les deux entreprises de la famille Peugeot sont fusionnées en 1910 sous le nom de « Société anonyme des automobiles et cycles Peugeot ».
+     *
+     * L'entreprise Peugeot est à l'origine du Groupe PSA qui englobe également Citroën, achetée à Michelin en 1976, DS, fondée en 2014, ainsi que Vauxhall et sa sœur allemande Opel (anciennement GM Europe).
+     *
+     * Peugeot produit essentiellement des véhicules automobiles pour particuliers, des utilitaires ainsi que des deux-roues. En 2016, la marque a vendu 1 919 460 véhicules4.
+     *
+     * Via son département Peugeot Sport, Peugeot a remporté notamment cinq championnats du monde des rallyes, deux championnats du monde des voitures de sport, deux coupes intercontinentales Le Mans Series d'endurance ainsi que sept Rallyes Raid Dakar.
+     */
+}
