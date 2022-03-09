49a36dcc311723f32d7b131d81a2d7cff9970996
Jean-Claude Tarby <jean-claude.tarby@univ-lille.fr>
2022-03-09 19:06:23 +0100

    ajout de l'Italie

---
+++ src/nouveaupackage/Italie.java
@@ -0,0 +1,19 @@
+package nouveaupackage;
+
+public class Italie {
+
+    private String unlongnomquineseerrtarien;
+
+    public Italie() {
+    }
+
+/**
+ * Histoire de l'Espagne (wikipedia)
+ * L'Italie (/itali/11 Écouter ; en italien : Italia /iˈtaːlja/12 Écouter), en forme longue la République italienne (en italien : Repubblica Italiana /reˈpubblika itaˈljaːna/12 Écouter), est un pays d'Europe du Sud correspondant physiquement à une partie continentale, une péninsule située au centre de la mer Méditerranée et une partie insulaire constituée par les deux plus grandes îles de cette mer, la Sicile et la Sardaigne, et beaucoup d'autres îles plus petites (hormis la Corse, française depuis 1768). Elle est rattachée au reste du continent par le massif des Alpes. Le territoire italien correspond approximativement à la région géographique homonyme.
+ *
+ * L'Italie apporte une contribution très importante à la civilisation occidentale : elle est notamment le berceau de la civilisation étrusque, de la Grande-Grèce, de l'Empire romain, du Saint-Siège, des républiques maritimes, de l'humanisme et de la Renaissance. Existant en tant qu'État unitaire depuis 1861 à la suite du Risorgimento (Renaissance ou Résurrection) mené par le royaume de Sardaigne, l'Italie est une république depuis l'abolition par référendum de la monarchie italienne en 1946. Elle est membre fondateur de l'Union européenne et de la zone euro.
+ *
+ * L'Italie est, en ce début de xxie siècle, une puissance moyenne13, forte de ses soixante millions d'habitants, de sa position en 2017 de huitième puissance économique mondiale et de troisième économie de la Zone Euro, et de son rôle au sein de nombreuses organisations internationales comme l'Union européenne, l'Organisation du traité de l'Atlantique nord, le G7 et l'Organisation de coopération et de développement économiques.
+ *
+ */
+}
