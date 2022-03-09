4c51b0baaeab9facb7649f94c207a45975537653
Jean-Claude Tarby <jean-claude.tarby@univ-lille.fr>
2022-03-09 18:23:00 +0100

    ajout de Peugeot

--- src/unpackage/Peugeot.java
+++ src/unpackage/Peugeot.java
@@ -1,6 +1,12 @@
 package unpackage;
 
 public class Peugeot {
+
+    private void unemethodevide()
+    {
+
+    }
+
     /**
      * Peugeot est un constructeur automobile français filiale du groupe Stellantis depuis 2021. L'entreprise familiale qui précède l'actuelle entreprise Peugeot est fondée en 18102, lors de la transformation du moulin familial sis à Hérimoncourt en aciérie. Différentes branches de la famille vont se lancer dans des productions très diversifiées, mais toutes basées sur l'acier (outils, ressorts, baleines de parapluies, armatures de corsets, moulins à café, pièces d'horlogerie, bicyclettes, etc.) À partir de 1886, Armand Peugeot commence à s'intéresser à l'automobile. En 18962, il crée la « Société anonyme des automobiles Peugeot »3. Parallèlement, ses neveux Pierre, Robert et Jules se lancent en 1897 dans la fabrication de cycles sous le nom « Les fils de Peugeot frères » et produisent en 1906 leur première automobile. Afin d'éviter une concurrence interne nuisible à la production, les deux entreprises de la famille Peugeot sont fusionnées en 1910 sous le nom de « Société anonyme des automobiles et cycles Peugeot ».
      *
