6cafb1d9b5a0fa6706856b004d95a85a1c0f3a69
Jean-Claude Tarby <jean-claude.tarby@univ-lille.fr>
2022-03-09 18:36:17 +0100

    ajout de l'Espagne

---
+++ src/nouveaupackage/Espagne.java
@@ -0,0 +1,24 @@
+package nouveaupackage;
+
+public class Espagne {
+
+    private String unlongnomquineseerrtarien;
+
+    public Espagne() {
+    }
+
+    /**
+     * Histoire de l'Espagne (wikipedia)
+     * L'Espagne, en forme longue le royaume d'Espagne (respectivement en espagnol : España Écouter et Reino de España), est un État souverain transcontinental d'Europe du Sud — et, selon les définitions, d'Europe de l'Ouest — qui occupe la plus grande partie de la péninsule Ibérique. En 2019, il s'agissait du vingt-huitième pays le plus peuplé du monde, avec 47 millions d'habitants.
+     *
+     * L'Espagne est bordée au nord-est par les Pyrénées, qui constituent une frontière naturelle avec la France et l'Andorre ; à l'est et au sud-est par la mer Méditerranée, au sud-sud-ouest par le territoire britannique de Gibraltar et le détroit du même nom, ce dernier séparant le continent européen de l'Afrique. Le Portugal est limitrophe de l'Espagne à l'ouest tandis que l'océan Atlantique borde le pays à l'ouest-nord-ouest ; enfin le golfe de Gascogne baigne le littoral nord. Le territoire espagnol inclut également les îles Baléares en Méditerranée, les îles Canaries dans l'océan Atlantique au large de la côte africaine, et deux cités autonomes en Afrique du Nord, Ceuta et Melilla, limitrophes du Maroc. Avec une superficie de 504 030 km2, l'Espagne est le pays le plus étendu d'Europe de l'Ouest et de l'Union européenne après la France ainsi que le troisième d'Europe derrière l'Ukraine et la France si l'on exclut la partie européenne (selon les définitions) de la Russie.
+     *
+     * Du fait de son emplacement, le territoire espagnol a été l'objet de nombreuses influences externes, souvent simultanément, depuis les temps préhistoriques jusqu'à la naissance de l'Espagne en tant que pays. Inversement, le pays lui-même a été une importante source d'inspiration pour d'autres régions, principalement durant l'ère moderne, lorsqu'il est devenu un empire colonial qui a laissé un héritage de près de 500 millions d'hispanophones à ce jour.
+     *
+     * L'Espagne en tant que pays est née de l'union dynastique au xve siècle de deux États souverains, les Couronnes de Castille et d'Aragon — elles-mêmes construites tout au long du Moyen Âge par l'union ou la conquête d'entités politiques, culturelles et linguistiques initialement distinctes, qui se retrouvent dans les multiples nationalités historiques reconnues par la Constitution actuelle de l'État espagnol — et de l'absorption en 1492 du royaume de Grenade et en 1512 de la partie ibérique du royaume de Navarre. Cet ensemble devient un État unitaire en 1715-1716 par la dissolution des deux Couronnes en application des décrets de Nueva Planta.
+     *
+     * La monarchie catholique espagnole, qui possède alors un immense empire colonial, est, du xve siècle au début du xviie siècle, une grande puissance politique et économique. Elle connaît notamment un important rayonnement culturel dans toute l'Europe durant le Siècle d'or espagnol (xvie siècle-xviie siècle). L'influence espagnole décline par la suite, particulièrement tout au long du xixe siècle et au début du xxe siècle avec la perte de ses colonies, la montée des nationalismes et la multiplication des crises politiques, économiques et sociales qui culminent avec la Guerre civile de 1936 à 1939 suivie d'une longue période de dictature franquiste, conservatrice, militariste et nationale catholique de 1939 à 1975.
+     *
+     * À la suite de la transition démocratique ouverte à la mort de Francisco Franco en 1975 et au mouvement culturel qui l'a accompagnée, la Movida, l'Espagne est devenue une monarchie constitutionnelle au régime démocratique parlementaire. C'est un pays développé doté de la quatorzième plus forte économie mondiale par PIB nominal7 (seizième à parité de pouvoir d'achat), et d'un niveau de vie « très élevé » (23e au classement IDH en 2012). C'est un membre de l'Organisation des Nations unies, de l'Union européenne, de l'Union latine, de l'OTAN, de l'OCDE et de l'OMC. En 2013, 21,6 % de la population espagnole vit dans la pauvreté8.
+     */
+}
